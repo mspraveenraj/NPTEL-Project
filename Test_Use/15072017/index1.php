25932ca27ba54885028143de255c80b1a5524b9a
Praveenraj <praveenrajpraveenraj007@gmail.com>
2017-07-20 18:27:53 +0530

    Add files via upload

---
+++ Test_Use/15072017/index1.php
@@ -0,0 +1,257 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<!--
+session_start();
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] != true) {
+    
+	header("location:index.php");
+	
+
+} 
+?>
+-->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>NPTEL - Web and Video Courses</title>
+<head>
+<style type="text/css">
+#linked
+{
+	color:#000;
+	text-decoration:none;
+}
+
+
+
+table { 
+  width: 100%; 
+    display: table;
+    border-collapse: separate;
+    border-spacing: 8px;
+    border-color: light-blue;
+}
+/* Zebra striping */
+tr:nth-of-type(odd) { 
+  background: #eee; 
+}
+th { 
+  background: #333; 
+  color: white; 
+  font-weight: bold; 
+}
+td, th { 
+  padding: 6px; 
+  border: 1px solid #ccc; 
+  text-align: left; 
+}
+
+#type1
+{
+	
+	
+	color:rgb(0,0,0);
+	font-family:"Times New Roman", Times, serif;
+	width:100px;
+	
+	cursor:pointer;
+	font-weight:bold;
+	
+	
+	
+	
+
+}
+
+#type1:hover
+{
+	color:#036;
+
+}
+
+#courselink
+{
+	text-decoration:none;
+	margin-right:15px;
+	font-size:18px;
+	color:#03C;
+	font-weight:bold;
+	float:right;
+}
+#courselink:hover
+{
+	color:#F00;
+}
+
+
+#top
+{
+	height:60px;
+	background-color:#FFF;
+	padding-top:5px;
+	padding-bottom:5px;
+}
+
+.button {
+    background-color: #4CAF50;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+}
+</style>
+</head>
+<body>
+
+<div>
+	<center><div id="center" style="position:relative; width:100%; height:100%">
+	<img src="images/img123.png">
+	</div></center></div>
+  <!--
+	<div id="right" style="float: right">
+
+	<a href="logout.php" class="button">Logout</a>
+	</div>
+
+<br />
+<br />
+-->
+<br /><br /><br />
+<br /><hr>
+
+<div align="center"><p style="font-size:21px; font-family:'Times New Roman', Times, serif; font-weight:bold; text-decoration:underline;">Discipline-Wise Listing</p></div>
+</br>
+	</br>
+	</br>
+	
+<table width="100%" border="2" width="100%" cellpadding="20" cellspacing="20">
+
+  <tr>
+
+    <td><a  href="aer1.php" id="linked" target="_blank" >Aerospace Engineering</a></td>
+
+    <td><a  href="atmospheric.php" id="linked" target="_blank">Atmospheric Science</a></td>
+
+    <td><a href="short/NPTEL/NPTEL Index/basic_courses.php" id="linked" target="_blank">Basic courses(Sem 1 and 2)</a></td>
+
+    <td><a href="biotech.php" id="linked" target="_blank">Biotechnology</a></td>
+
+  </tr>
+
+  <tr>
+
+    
+
+    <td><a href="short/NPTEL/NPTEL Index/chemical.php" id="linked" target="_blank">Chemical Engineering</a></td>
+
+    <td><a href="chem_bioichem.php" id="linked" target="_blank">Chemistry and Biochemistry</a></td>
+
+    <td><a href="civil.php" id="linked" target="_blank">Civil Engineering</a></td>
+
+    <td><a  href="short/NPTEL/NPTEL Index/css.php" id="linked" target="_blank">Computer Science and Engineering</a></td>
+
+  </tr>
+
+  <tr>
+
+    
+
+    <td><a href="electrical.php" id="linked" target="_blank">Electrical Engineering</a></td>
+
+    <td><a href="short/NPTEL/NPTEL Index/ece.php" id="linked" target="_blank">Electronics & Communication Engineering</a></td>
+
+    <td><a href="short/NPTEL/NPTEL Index/engg_design.php" id="linked" target="_blank">Engineering Design</a></td>
+
+     <td><a href="enviorm.php" id="linked" target="_blank">Environmental Science </a></td>
+
+  </tr>
+
+  <tr>
+
+   
+
+    <td><a href="general.php" id="linked" target="_blank">General</a></td>
+
+    <td><a href="HSS.php" id="linked" target="_blank">Humanities and Social Sciences</a></td>
+
+    <td><a href="short/NPTEL/NPTEL Index/management.php" id="linked" target="_blank">Management</a></td>
+
+     <td><a href="maths.php" id="linked" target="_blank">Mathematics</a></td>
+
+  </tr>
+
+  <tr>
+
+   
+
+    <td><a href="mechanical.php" id="linked" target="_blank">Mechanical Engineering</a></td>
+
+    <td><a href="metallurgy.php" id="linked" target="_blank">Metallurgy and Material Science </a></td>
+
+    <td><a href="short/NPTEL/NPTEL Index/mining.php" id="linked" target="_blank">Mining Engineering</a></td>
+
+     <td><a href="nano.php" id="linked" target="_blank">Nanotechnology</a></td>
+
+  </tr>
+
+  <tr>
+
+   
+
+    <td><a href="ocean.php" id="linked" target="_blank">Ocean Engineering</a></td>
+
+    <td><a href="physics.php" id="linked" target="_blank">Physics</a></td>
+
+    <td><a href="textile.php" id="linked" target="_blank">Textile Engineering </a></td>
+
+  </tr>
+
+
+
+
+</table>
+
+   <style>
+	   
+	   
+	   a:hover{
+			background-color: #66C3FC;
+		}
+       </style>
+	   
+	   <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<!--<script>
+    
+$(window).unload(function(){
+    $.ajax({
+        type: 'POST',
+        url: 'logout.php',
+        async:false,
+        data: {key_leave:"289583002"}
+    });
+});	
+	-->
+	  
+
+
+	</br>
+    </br>
+	</br>
+	</br>
+	</br>
+	</br>
+	</br>
+	</br>
+	</br>
+	
+	
+	<hr><center><font size="4" color="red">DEVELOPED BY </font><font size="4" color="green"><b>- Dhanush.N,  Praveenraj.M.S,  Surendran.N,  Prem Kumar.M,  Induja.M,  Keerthana.K - CSE 3rd YEAR - BATCH 2014 - 2018<b></font></center></hr>
+	<hr></hr>
+	</br>
+</body>
+</html>
---
+++ Test_Use/test/index_last.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+<link rel="icon" href="images/favicon.png" type="image/png"/>
+<link rel="shortcut icon" href="images/favicon.png" type="image/png"/>
+<title>CsePro</title>
+<style>
+.div1 {
+
+  width:250px;
+height:180px;
+padding:30px;
+border: 1px solid black;
+border-radius:25px;	
+background-color:#f1f1c1;
+}
+
+body  {
+    background-image: url("images/bgimgnew.jpg");
+    background-color: #cccccc;
+
+background-color:#fffff0;
+}
+b{
+font-size:250%;
+color:darkred;
+}
+div{
+color:brown;
+}
+</style>
+</head>
+<body>
+<br>
+
+<center>
+<table>
+<tr>
+<td> <img src="images/logo.jpg" alt="no img" width="100px" height="100px"></td>
+<td> <center><b>K.S.R. College of Engineering</b></center><div align="center">(AUTONOMOUS)</div></td>
+<td> <img src="images/123456.png" alt="no img" width="100px" height:=100px"></td>
+</tr>
+</table>
+</center>
+<br><br><br><br>
+<form  name="form1" method="post" action="check_login.php">
+<center>
+<div class="div1">
+<h4>Login Form</h4>
+User Name:<input name="myusername" type="text" id="myusername"/><br><br>
+Password :<input name="mypassword" type="password" id="mypassword"/><br><br>
+<input type="submit"name="Submit" value="Login"/>
+</div>
+</center>
+</form>
+</body>
